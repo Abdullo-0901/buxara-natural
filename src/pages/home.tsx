82692123c99a5eade0c64026c25fb58288ec255a
Olimov Abdulloh <lochin.tj@gmail.com>
2024-04-08 18:24:33 +0500

    add position

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,23 @@
+import { useTranslation } from "react-i18next";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="">
+      <section className="bg-[#464342]  h-[90vh] pr-1 items-center flex justify-between  rounded-3xl w-full relative ">
+        <img className="absolute right-0  " src="/home.png" alt="" />
+
+        <div>
+          <h1 className="text-[81px] text-white font-bold">
+            {t("hometitle1")}
+          </h1>
+          <h1>{t("hometitle2")}</h1>
+          <h1>{t("hometitle3")}</h1>
+        </div>
+      </section>
+      <div>ljkhn</div>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import { HeaderMenu } from "../components/Header/headers";
 
 const Layout = () => {
   return (
     <div>
       <HeaderMenu />
+      <div className="max-w-[1450px] overflow-hidden p-[0_8px] m-[0_auto] mt-2">
+        <Outlet />
+      </div>
     </div>
   );
 };

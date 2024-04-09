9111a6c647b420e8433b8bcc7e9f20f4d3662f09
Olimov Abdulloh <lochin.tj@gmail.com>
2024-04-09 11:08:35 +0500

    add size text-and home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,15 +4,23 @@ const Home = () => {
   const { t } = useTranslation();
   return (
     <div className="">
-      <section className="bg-[#464342]  h-[90vh] pr-1 items-center flex justify-between  rounded-3xl w-full relative ">
-        <img className="absolute right-0  " src="/home.png" alt="" />
+      <section className="bg-[#464342] sm:h-auto sm2:h-[30vh] mmd:h-[45vh]  sm3:h-[40vh] md:h-[65vh] lg:h-[90vh] pr-1 items-center flex justify-end  rounded-3xl w-full relative ">
+        <img
+          className="absolute right-0 w-full -left-[22%]"
+          src="/home.png"
+          alt=""
+        />
 
         <div>
-          <h1 className="text-[81px] text-white font-bold">
+          <h1 className="  md3:text-[68px] sm3:text-[32px] sm:text-[22px]  text-white font-bold">
             {t("hometitle1")}
           </h1>
-          <h1>{t("hometitle2")}</h1>
-          <h1>{t("hometitle3")}</h1>
+          <h1 className="md3:text-[108px] sm3:text-[42px] sm:text-[32px] text-white font-bold">
+            {t("hometitle2")}
+          </h1>
+          <h1 className="md3:text-[118px] sm3:text-[5 tex2px] sm:text-[42px] text-white font-bold">
+            {t("hometitle3")}
+          </h1>
         </div>
       </section>
       <div>ljkhn</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,32 @@ export default {
   theme: {
     extend: {},
   },
+  theme: {
+    screens: {
+      sm: "300px",
+      // => @media (min-width: 640px) { ... }
+      sm2: "400px",
+      sm3: "451px",
+      // => @media (min-width: 640px) { ... }
+
+      mmd: "550px",
+      md: "768px",
+
+      md2: "820px",
+      // => @media (min-width: 768px) { ... }
+
+      md3: "900px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
   plugins: [],
 };

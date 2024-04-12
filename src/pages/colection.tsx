7de1ae3b2a0e153ca616aa37147375286b2e0fe2
Olimov Abdulloh <lochin.tj@gmail.com>
2024-04-12 12:01:40 +0500

    add collection

--- src/pages/colection.tsx
+++ src/pages/colection.tsx
@@ -1,5 +1,10 @@
 const Colection = () => {
-  return <div>Colection</div>;
+  return (
+    <div className="grid grid-cols-6">
+      <div className="col-span-2"></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Colection;
